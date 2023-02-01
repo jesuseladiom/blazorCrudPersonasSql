5c8b205d6e0d1b84fc3830b22aa0a9a73ced2997
jesuseladiom <jesuseladiom@gmail.com>
2023-02-01 10:40:07 -0700

    sweetAlert

---
+++ Client/Helpers/JSextensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorCrudPersonasSql.Client.Helpers
+{
+    public static class JSextensions
+    {
+        public static async Task<object> MostrarMensaje(this IJSRuntime js, string mensaje)
+        {
+            return await js.InvokeAsync<object>("Swal.fire", mensaje);
+        }
+
+        public static async Task<object> MostrarMensaje(this IJSRuntime js, string titulo, string mensaje, TipoMensajeSweetAlert tipoMensajeAlert)
+        {
+            return await js.InvokeAsync<object>("Swal.fire", titulo, mensaje, tipoMensajeAlert.ToString());
+        }
+
+        public enum TipoMensajeSweetAlert  
+        {
+            question, warning, error, success, info
+        }
+
+        public static async Task<bool> Confirmar(this IJSRuntime js, string titulo, string mensaje, TipoMensajeSweetAlert tipoMensajeAlert)
+        {
+            return await js.InvokeAsync<bool>("CustomConfirm", titulo, mensaje, tipoMensajeAlert.ToString());
+        }
+    }
+}
---
+++ Client/wwwroot/js/Utilidades.js
@@ -0,0 +1,18 @@
+﻿function CustomConfirm(titulo, mensaje, tipo) {
+    return new Promise((resolve) => {
+        Swal.fire({
+            title: titulo,
+            text: mensaje,
+            icon: tipo,
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Confirmar'
+        }).then((result) => {
+            if (result.isConfirmed)
+                resolve(true)
+            else
+                resolve(false)
+        });
+    });
+}
