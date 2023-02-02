4245d63dc3260c1d15db5fc1ff3b0b06fbdb31b4
jesuseladiom <jesuseladiom@gmail.com>
2023-02-02 12:05:55 -0700

    autenticar

---
+++ Client/Auth/AuthStateProviderFalso.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Reflection;
+using System.Security.Claims;
+
+namespace BlazorCrudPersonasSql.Client.Auth
+{
+    
+    public class AuthStateProviderFalso : AuthenticationStateProvider
+    {
+        // este metodo determina si el usuario esta autenticado
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            //var identity = new ClaimsIdentity(new List<Claim>
+            //{
+            //    new Claim(ClaimTypes.Name, "Felipe")
+            //}, "prueba");
+            var identity = new ClaimsIdentity();
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(identity)));
+
+
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,6 @@
 using BlazorCrudPersonasSql.Client;
+using BlazorCrudPersonasSql.Client.Auth;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +9,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// para usar authenticacion
+builder.Services.AddAuthorizationCore();
+//agregar mi clase de autenticacion
+builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProviderFalso>();  
 
 await builder.Build().RunAsync();
